fabfe82ae7343b957174f78bbb03f5f1821036df
arturojreal <arturojreal@users.noreply.github.com>
2015-09-03 20:07:17 -0400

    First finished pset

--- psets/0.js
+++ psets/0.js
@@ -11,3 +11,10 @@ Why pay a fortune teller when you can just program your fortune yourself?
 */
 
 // write your solution here...
+
+var numOfChildren = 1;
+var partnerName = "Lauren";
+var geoLocation = "Miami";
+var jobTitle = "Hacker";
+
+console.log("You will be a " + jobTitle + "in " + geoLocation + ", and married to " + partnerName + "with " + numOfChildren + "kids");
