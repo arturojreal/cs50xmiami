7d71d55d2de490466aee7c881ab153211e7f41e4
arturojreal <arturojreal@users.noreply.github.com>
2015-09-03 20:48:19 -0400

    Finished pset1

--- psets/1.js
+++ psets/1.js
@@ -10,3 +10,10 @@ Forgot how old someone is? Calculate it!
 */
 
 // write your solution here...
+
+
+var currentYear = 2015;
+var birthYear = prompt("What year were you born in? (Example: 1994)");
+var age1 = currentYear - birthYear;
+var age2 = currentYear - birthYear - 1;
+alert(" They are either" + age1 + " or " + age2 " ");
